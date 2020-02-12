99d0ef2829dc8a866e485e24d915b24265137dac
Duane Creates <duanecreates@gmail.com>
2020-02-12 19:25:46 +0100

    Add CSS reset & base

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 // This is the main.js file. Import global CSS and scripts here.
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 
+import 'normalize.css/normalize.css'
+import '~/assets/base.scss'
 import '~/assets/global.scss'
 import DefaultLayout from '~/layouts/Default.vue'
 
